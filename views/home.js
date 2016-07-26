4a5147be77b74c8b62b58f57b4cbb545e01b3a70
denisinla <denis@reactiveventur.es>
2016-07-25 22:53:18 -0700

    Update: adjusted ES6 style module.exports for classes.

--- views/home.js
+++ views/home.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from '../components/hello';
 
-class Home extends React.Component{
+export default class Home extends React.Component{
   render(){
     return (
       <Hello />
     )
   }
 }
-
-module.exports = Home;
